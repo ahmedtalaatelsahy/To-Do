0e520447a91699ef4a8439c140e9459dec944390
PC <ahmedelsahy236@mail.com>
2024-11-08 23:55:39 +0200

    +finish register screen +finish login screen +apply firebase auth +apply provider

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_app/providers/auth_provider.dart';
 import 'package:to_do_app/ui/home/settings/settings_tab.dart';
+import 'package:to_do_app/ui/login/login_screen.dart';
 
 import 'list/tasks_list_tab.dart';
 
@@ -18,12 +21,24 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var authProvider = Provider.of<AppAuthProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           '   To Do List',
           style: Theme.of(context).appBarTheme.titleTextStyle,
         ),
+        actions: [
+          InkWell(
+              onTap: () {
+                authProvider.logout();
+                Navigator.pushReplacementNamed(context, LoginScreen.routeName);
+              },
+              child: Icon(Icons.logout_rounded)),
+          SizedBox(
+            width: 10,
+          ),
+        ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
